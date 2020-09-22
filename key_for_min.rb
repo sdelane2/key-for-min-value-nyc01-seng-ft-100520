66381425827beaff648539bfe71b5d04320d8a7f
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-22 21:01:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return NIL
   elsif name_hash.each do |key, value|
-    if new_value < value 
+    if new_value == nil || value 
       new_value = value
       new_key = key
     end
